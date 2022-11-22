206e2bfe8cf42637136f4eb4ad63c0ec8f027220
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-11-22 11:17:47 -0700

    get all test failed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,26 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#GET should return a list of all Amandas pets', async () => {
+    const res = await request(app).get('/pets');
+    expect(res.body).toEqual(expect.arrayContaining([
+      expect.objectContaining(
+        {
+          id: expect.any(String),
+          name: expect.any(String),
+          species: expect.any(String),
+          breed: expect.any(String),
+          age: expect.any(Number)
+        }
+      )
+    ])
+    );
   });
   afterAll(() => {
     pool.end();
